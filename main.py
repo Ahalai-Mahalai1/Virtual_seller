2b41d18ba7c68147a3b1c55d1b5a184ad3ba8044
Andrey <andreymalakhov2612@gmail.com>
2025-09-10 18:01:08 +0300

    First

---
+++ main.py
@@ -0,0 +1,25 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from config.config import Config, load_config
+from handlers import user_handlers, other_handlers, admin_handlers
+from aiogram import types
+# Функция конфигурирования и запуска бота
+async def main() -> None:
+
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
+
+    # Инициализируем бот и диспетчер
+    bot = Bot(token=config.bot.token)
+    dp = Dispatcher()
+    dp.include_router(user_handlers.router)
+    dp.include_router(admin_handlers.router)
+    dp.include_router(other_handlers.router)
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    #await reply_markup = types.ReplyKeyboardRemove()
+
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
